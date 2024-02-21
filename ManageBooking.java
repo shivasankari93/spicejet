4af6d0bffa3823fd293811cd3a52384bc5c1da91
shivasankari93 <shivasankariofficial@gmail.com>
2024-02-21 23:20:28 +0530

    commit

---
+++ ManageBooking.java
@@ -0,0 +1,31 @@
+package page.component;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import test.component.Base;
+
+public class ManageBooking extends Base{
+	@FindBy (xpath = "//div[@class='css-1dbjc4n r-1d09ksm r-qklmqi r-1lz4bg0 r-1phboty r-18u37iz']/div[4]")
+	WebElement manage;
+	@FindBy (xpath = "(//div[@class=\"css-1dbjc4n r-18u37iz r-1ifxtd0 r-6ity3w\"]/div/div)[2]/input")
+	WebElement ticketNumber;
+	@FindBy (xpath = "//div[@class=\"css-1dbjc4n r-18u37iz r-1ifxtd0 r-6ity3w\"]/div[2]/div[2]/input")
+	WebElement email;
+	@FindBy (xpath = "//div[@class=\"css-1dbjc4n r-1pcd2l5 r-1uwte3a\"]/div[3]")
+	WebElement searchBooking;
+	public ManageBooking() {
+		PageFactory.initElements(driver, this);
+	}
+	public void ManageBookingMethod() {
+		clickEvent(manage);
+		threadSleepmethod();
+		keyInput(ticketNumber, "W3X3H8");
+		threadSleepmethod();
+		keyInput(email, "shivasankari1093@gmail.cm");
+		threadSleepmethod();
+		clickEvent(searchBooking);
+		threadSleepmethod();
+	}
+}
